6fb406ae068567f32dac61a6ad8ee9311fbad252
MartinBriefeneder <m.briefeneder@htblaleonding.onmicrosoft.com>
2025-10-23 13:42:40 +0200

    Move upload to api resource

--- src/main/java/at/htl/model/ApiResource.java
+++ src/main/java/at/htl/model/ApiResource.java
@@ -4,13 +4,19 @@ import at.htl.entity.Category;
 import at.htl.entity.Image;
 import at.htl.entity.Party;
 import at.htl.entity.User;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +35,7 @@ public class ApiResource {
     public List<User> getUsers() {
         return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
     }
+
     @GET
     @Path("/categories")
     public List<Category> getCategory() {
@@ -51,13 +58,42 @@ public class ApiResource {
     @Path("/images/{party}/")
     public List<Image> getImages(@PathParam("party") long partyId) {
         List<Image> result = new ArrayList<>();
-       boolean access = entityManager.createQuery("SELECT userId FROM PartyAttendees WHERE partyId=" + partyId).getResultList().stream().findFirst().isPresent();
-       logger.log(Logger.Level.INFO, "access? " + access);
+        boolean access = entityManager.createQuery("SELECT userId FROM PartyAttendees WHERE partyId=" + partyId).getResultList().stream().findFirst().isPresent();
+        logger.log(Logger.Level.INFO, "access? " + access);
 
-       if (access) {
-           result = entityManager.createQuery("SELECT url FROM Image WHERE party_id=" + partyId).getResultList();
-       }
-       return result;
+        if (access) {
+            result = entityManager.createQuery("SELECT url FROM Image WHERE party_id=" + partyId).getResultList();
+        }
+        return result;
     }
 
+    @POST
+    @Path("/upload/{partyId}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response upload(FileUploadInput input, @PathParam("partyId") long partyId) throws IOException {
+        // TODO: put into db
+        logger.log(Logger.Level.DEBUG, "upload");
+        String uploadDir = "src/main/resources/uploads/party" + partyId + "/";
+        Files.createDirectories(Paths.get(uploadDir));
+        for (FileUpload file : input.file) {
+            java.nio.file.Path uploadedFile = file.uploadedFile();
+            java.nio.file.Path targetLocation = Paths.get(uploadDir, file.fileName());
+            Files.move(uploadedFile, targetLocation);
+            logger.log(Logger.Level.INFO, "File saved to: " + targetLocation);
+        }
+        return Response.ok().build();
+    }
+
+    public static class FileUploadInput {
+
+        @FormParam("text")
+        public String text;
+
+        @FormParam("file")
+        public List<FileUpload> file;
+
+    }
 }
+
+
