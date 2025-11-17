de499a02dd0e7ad67141c6c993cf64609788e230
MartinBriefeneder <m.briefeneder@htblaleonding.onmicrosoft.com>
2025-11-17 10:14:19 +0100

    Attend; Entities; Uml;

--- src/main/java/at/htl/entity/PartyAttendees.java
+++ src/main/java/at/htl/entity/PartyAttendees.java
@@ -11,4 +11,28 @@ public class PartyAttendees {
     @Id
     @Column(name = "user_id")
     private Long userId;
+
+    public PartyAttendees(){
+
+    }
+    public PartyAttendees(Long partyId, Long userId) {
+        setPartyId(partyId);
+        setUserId(userId);
+    }
+
+    public Long getPartyId() {
+        return partyId;
+    }
+
+    public void setPartyId(Long partyId) {
+        this.partyId = partyId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
