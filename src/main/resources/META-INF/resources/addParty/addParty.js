7165adac4ebcab1ae5dce709dec5cd2f957cbfe4
annar2703 <anna.reder2703ar@gmail.com>
2025-12-01 11:12:26 +0100

    Refactor addParty styles and scripts for improved structure and clarity

--- src/main/resources/META-INF/resources/addParty/addParty.js
+++ src/main/resources/META-INF/resources/addParty/addParty.js
@@ -1,18 +1,18 @@
-/* Datum css bei jedem Browser gleich zu machen */
+// Extra JS Datei – nur hier dein JS-Code
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Flatpickr Typ:", typeof flatpickr); // Debug in Konsole
 
-document.addEventListener('DOMContentLoaded', () => {
   flatpickr("#time_start", {
     enableTime: true,
     dateFormat: "d.m.Y H:i",
     time_24hr: true,
-    minuteIncrement: 15
+    minuteIncrement: 15,
   });
 
   flatpickr("#time_end", {
     enableTime: true,
     dateFormat: "d.m.Y H:i",
     time_24hr: true,
-    minuteIncrement: 15
+    minuteIncrement: 15,
   });
 });
-
