e4ce3105dc96c055f8b67948260125acf28823dd
Carla Dimmler <c.dimmler@students.htl-leonding.ac.at>
2025-12-03 11:01:43 +0100

    #35

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-docker build --tag ghcr.io/htl-leo-itp-25-27-4-5bhitm/partyhub-app --file "./src/main/docker/Dockerfile" .
+mvn clean package -DskipTests
+docker buildx build --platform linux/amd64 --tag ghcr.io/htl-leo-itp-25-27-4-5bhitm/partyhub-app --file "./src/main/docker/Dockerfile" .
 docker push ghcr.io/htl-leo-itp-25-27-4-5bhitm/partyhub-app
 
