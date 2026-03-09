9a5081cfccb9576b84b66f2dcc63f5f36272533b
Carla Dimmler <c.dimmler@students.htl-leonding.ac.at>
2026-03-09 19:35:53 +0100

    #74, 73

---
+++ PartyHubiOS/PartyHubiOS/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  PartyHubiOS
+//
+//  Created by Carla Dimmler on 09.03.26.
+//
+
+import Foundation
---
+++ PartyHubiOS/PartyHubiOS/PartyDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PartyDetailView.swift
+//  PartyHubiOS
+//
+//  Created by Carla Dimmler on 09.03.26.
+//
+
+import SwiftUI
+
+struct PartyDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PartyDetailView()
+}
