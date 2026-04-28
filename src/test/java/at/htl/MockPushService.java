fa75f4f8bf760e96c9d1d889d19de3993323b80d
it220214 <c.dimmler@students.htl-leonding.ac.at>
2026-04-28 12:47:04 +0200

    editt

--- src/test/java/at/htl/MockPushService.java
+++ src/test/java/at/htl/MockPushService.java
@@ -2,9 +2,12 @@ package at.htl;
 
 import io.quarkus.test.Mock;
 import jakarta.enterprise.context.ApplicationScoped;
-
+import jakarta.enterprise.inject.Alternative;
+import jakarta.annotation.Priority;
 
 @Mock
+@Alternative  // <--- Hinzufügen
+@Priority(1)  // <--- Hinzufügen (irgendeine Zahl > 0)
 @ApplicationScoped
 public class MockPushService extends PushNotificationService {
     @Override
