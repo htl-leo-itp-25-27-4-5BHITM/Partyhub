3f859ff85b2857fe85e21f5219565d35400d0d46
it220214 <c.dimmler@students.htl-leonding.ac.at>
2026-04-28 12:35:08 +0200

    mock respurce

---
+++ src/main/java/at/htl/MockPushService.java
@@ -0,0 +1,13 @@
+package at.htl;
+
+import io.quarkus.test.Mock;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@Mock
+@ApplicationScoped
+public class MockPushService extends PushNotificationService {
+    @Override
+    public void notifyParticipants(Long partyId, String message) {
+        System.out.println("CI-Test: Push unterdrückt für Party " + partyId);
+    }
+}
