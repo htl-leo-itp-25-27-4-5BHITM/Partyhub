d5e3afe7c5e9a479273a69406471a916768cbb3c
it220214 <c.dimmler@students.htl-leonding.ac.at>
2026-05-05 16:00:11 +0200

    added pp

--- PartyHubiOS/PartyHubiOS/Config.swift
+++ PartyHubiOS/PartyHubiOS/Config.swift
@@ -1,5 +1,5 @@
 enum Config {
-    //static let backendURL = "http://localhost:8080"
+    static let backendURL = "http://localhost:8080"
     //static let backendURL = "http://it220214.cloud.htl-leonding.ac.at"
-    static let backendURL = "https://it220274.cloud.htl-leonding.ac.at"
+    //static let backendURL = "https://it220274.cloud.htl-leonding.ac.at"
 }
